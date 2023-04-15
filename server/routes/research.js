238ad7fedd56f9b37b80eff52bbd2c573e75dd79
Hamees Sayed <98336593+hamees-sayed@users.noreply.github.com>
2023-04-15 14:42:51 +0530

    Update research.js

--- server/routes/research.js
+++ server/routes/research.js
@@ -3,6 +3,6 @@ const router = express.Router()
 
 const research_controller = require("../controllers/researchController")
 
-router.get("/", research_controller.get_research_list)
+router.get("/research", research_controller.get_research_list)
 
 module.exports = router;
